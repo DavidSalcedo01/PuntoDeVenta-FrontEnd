a9e6548b862692679e25d1698455450763319d86
DavidSalcedo01 <a22110109@ceti.mx>
2024-05-10 16:41:18 -0600

    Fetch al endpoint desprotegido

--- src/Components/Form.tsx
+++ src/Components/Form.tsx
@@ -7,7 +7,23 @@ const logInData = {
     email: "a22110109@ceti.mx",
     password: "12345"
 }
+const logIn = async ({email, password}: {email: string, password: string}) => {
+    try{
 
+        const response = await fetch(`${API_URL}api/v1/auth/login`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({email, password})
+        })
+
+        const data = await response.json()
+        console.log(data)
+    }catch(error){
+        console.log(error)
+    }
+}
 
 function Form(){
     const [email, setUsername] = useState<string>("")
@@ -21,13 +37,13 @@ function Form(){
 
     const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
-    
-        if (email !== logInData.email || password !== logInData.password) {
+        logIn({email, password})
+        /*if (email !== logInData.email || password !== logInData.password) {
             alert("Correo electrónico o contraseña incorrectos");
             return;
         }
 
-        alert("Datos correctos. Puedes proceder.");
+        alert("Datos correctos. Puedes proceder.");*/
     };
 
 
